0ca9d19bac0a429620f1191178078cbc2f5caecb
Bedi Gupta <Bedigupta76@gmail.com>
2023-08-25 10:48:24 +0530

    Completed this project using JavaScript Libraries
    After making this project I get to know my level of understanding of things where I am weak and where I have to work more

---
+++ front-end/index.js
@@ -0,0 +1,11 @@
+document.getElementById("generateButton").addEventListener("click", function() {
+    var inputText = document.getElementById("inputText").value;
+    
+    if (inputText !== "") {
+        var qrcode = new QRCode(document.getElementById("qrcode"), {
+            text: inputText,
+            width: 128,
+            height: 128
+        });
+    }
+});
